d57cc54741557cda3b31e4ae7de8a9d5582cf096
Kumudu Gunasekara <kumudug1@gmail.com>
2018-12-09 14:36:25 -0800

    Rest service

--- Friends/src/main/java/com/companyname/Friends/controller/FriendController.java
+++ Friends/src/main/java/com/companyname/Friends/controller/FriendController.java
@@ -1,12 +1,33 @@
 package com.companyname.Friends.controller;
 
+import com.companyname.Friends.model.Friend;
 import com.companyname.Friends.service.FriendService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class FriendController {
 
     @Autowired
     FriendService friendService;
+
+    @PostMapping("/friend")
+    Friend create(@RequestBody Friend friend) {
+        return friendService.save(friend);
+    }
+
+    @GetMapping("/friend")
+    Iterable<Friend> read() {
+        return friendService.findAll();
+    }
+
+    @PutMapping("/friend")
+    Friend update(@RequestBody Friend friend) {
+        return friendService.save(friend);
+    }
+
+    @DeleteMapping("/friend/{id}")
+    void delete(@PathVariable Integer id) {
+        friendService.deleteById(id);
+    }
 }
