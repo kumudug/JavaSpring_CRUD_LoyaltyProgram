947a5c5a609725ea11f9e7e03c16660c4b574744
Kumudu Gunasekara <kumudug1@gmail.com>
2018-12-09 23:27:02 -0800

    Search API

--- Friends/src/main/java/com/companyname/Friends/controller/FriendController.java
+++ Friends/src/main/java/com/companyname/Friends/controller/FriendController.java
@@ -5,6 +5,8 @@ import com.companyname.Friends.service.FriendService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 public class FriendController {
 
@@ -29,5 +31,17 @@ public class FriendController {
     @DeleteMapping("/friend/{id}")
     void delete(@PathVariable Integer id) {
         friendService.deleteById(id);
+    }
+
+    @GetMapping("/friend/{id}")
+    Optional<Friend> findById(@PathVariable Integer id) {
+        return friendService.findById(id);
+    }
+
+    @GetMapping("/friend/search")
+    Iterable<Friend> findByQuery(
+            @RequestParam("first") String firstName,
+            @RequestParam("last") String lastName) {
+        return friendService.findByFirstNameAndLastName(firstName, lastName);
     }
 }
--- Friends/src/main/java/com/companyname/Friends/service/FriendService.java
+++ Friends/src/main/java/com/companyname/Friends/service/FriendService.java
@@ -4,4 +4,6 @@ import com.companyname.Friends.model.Friend;
 import org.springframework.data.repository.CrudRepository;
 
 public interface FriendService extends CrudRepository<Friend, Integer> {
+    //Spring Data JPA will generate code for us
+    Iterable<Friend> findByFirstNameAndLastName(String firstName, String lastName);
 }
