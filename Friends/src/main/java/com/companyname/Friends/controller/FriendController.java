f5edd9092b0a8bbb24d38898959b64b5a0ce0cfc
Kumudu Gunasekara <kumudug1@gmail.com>
2018-12-13 08:07:25 -0800

    Error handling with @ErrorHandler annotation

--- Friends/src/main/java/com/companyname/Friends/controller/FriendController.java
+++ Friends/src/main/java/com/companyname/Friends/controller/FriendController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.bind.ValidationException;
 import java.util.Optional;
 
 @RestController
@@ -16,8 +17,17 @@ public class FriendController {
     FriendService friendService;
 
     @PostMapping("/friend")
-    Friend create(@RequestBody Friend friend) {
-        return friendService.save(friend);
+    Friend create(@RequestBody Friend friend) throws Exception {
+        if(friend.getId() == 0 && friend.getFirstName() != null && friend.getLastName() != null) {
+            return friendService.save(friend);
+        } else {
+            throw new ValidationException("Frind cannot be created");
+        }
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    ResponseEntity<String> exceptionHandler(ValidationException e) {
+        return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @GetMapping("/friend")
